5cfb256066c62cb553ec70e2a2e1774fc45d4f96
pavanbanka-417 <pavansaibanka08@gmail.com>
2025-05-18 11:19:43 +0530

    Add files via upload

---
+++ ML- journey google day 1 .py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+pet = input("what is the name of your pet ?")
+city = input("what is the name of the city you grew up in ?")
+print("The name of your band is", pet +" "+ city)
